c2d3d0f05d3f89170e775836e4e582d909de787e
young geun Song <pangsezi@gmail.com>
2024-03-14 08:04:13 +0000

    initial

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# extending_cdsview_ui5
-extending_cdsview ui5 source(bas)
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Wed Mar 13 2024 08:18:46 GMT+0000 (Coordinated Universal Time)|
+|**App Generator**<br>@sap/generator-fiori-elements|
+|**App Generator Version**<br>1.12.5|
+|**Generation Platform**<br>SAP Business Application Studio|
+|**Template Used**<br>Analytical List Page V2|
+|**Service Type**<br>SAP System (ABAP Environment on SAP Business Technology Platform)|
+|**Service URL**<br>https://1374dadc-372f-46e3-a692-b2bd57ae0ee4.abap.us10.hana.ondemand.com/sap/opu/odata/sap/ZUI_FE_BOOKING_001062_O2
+|**Module Name**<br>bookinganalysis|
+|**Application Title**<br> Booking Analysis|
+|**Namespace**<br>sap.fe.demo|
+|**UI5 Theme**<br>sap_horizon|
+|**UI5 Version**<br>1.120.9|
+|**Enable Code Assist Libraries**<br>False|
+|**Enable TypeScript**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Main Entity**<br>Booking|
+|**Navigation Entity**<br>None|
+
+## bookinganalysis
+
+An SAP Fiori application. -  Booking Analysis
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply run the following from the generated app root folder:
+
+```
+    npm start
+```
+
+- It is also possible to run the application using mock data that reflects the OData Service URL supplied during application generation.  In order to run the application with Mock Data, run the following from the generated app root folder:
+
+```
+    npm run start-mock
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ webapp/ext/controller/AnalyticalListPageExt.controller.js
@@ -0,0 +1,54 @@
+sap.ui.define([
+    "sap/ui/model/Filter", 
+    "sap/ui/comp/smartfilterbar/SmartFilterBar", 
+    "sap/m/ComboBox"
+], function (Filter, SmartFilterBar, ComboBox) {
+    "use strict";
+    return {
+        getCustomAppStateDataExtension: function (oCustomData) {
+            //the content of the custom field will be stored in the app state, so that it can be restored later, for example after a back navigation.
+            //The developer has to ensure that the content of the field is stored in the object that is passed to this method.
+            if (oCustomData) {
+                var oCustomField1 = this.oView.byId("CurrencyCode");
+                if (oCustomField1) {
+                    oCustomData.CurrencyCode = oCustomField1.getSelectedKey();
+                }
+            }
+        },
+        restoreCustomAppStateDataExtension: function (oCustomData) {
+            //in order to restore the content of the custom field in the filter bar, for example after a back navigation,
+            //an object with the content is handed over to this method. Now the developer has to ensure that the content of the custom filter is set to the control
+            if (oCustomData) {
+                if (oCustomData.CurrencyCode) {
+                    var oComboBox = this.oView.byId("CurrencyCode");
+                    oComboBox.setSelectedKey(
+                        oCustomData.CurrencyCode
+                    );
+                }
+            }
+        },
+        onBeforeRebindTableExtension: function(oEvent) {
+            var oBindingParams = oEvent.getParameter("bindingParams");
+            oBindingParams.parameters = oBindingParams.parameters || {};
+
+            var oSmartTable = oEvent.getSource();
+            var oSmartFilterBar = this.byId(oSmartTable.getSmartFilterId());
+            if (oSmartFilterBar instanceof SmartFilterBar) {
+                var oCustomControl = oSmartFilterBar.getControlByKey("fragCurrencyKey");
+                if (oCustomControl instanceof ComboBox) {
+                    var vCategory = oCustomControl.getSelectedKey();
+                    switch (vCategory) {
+                        // case "0" :
+                        // 	oBindingParams.filters.push(new Filter("Supplier", "EQ", "SAP"));
+                        // 	break;
+                        // case "1" :
+                        // 	oBindingParams.filters.push(new Filter("Supplier", "EQ", "OTHERS"));
+                        // 	break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+    };
+});
